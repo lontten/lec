9589fbc06dd026fa3f3a692e8c8dae603c2aec47
lontten <lontten@163.com>
2023-08-26 16:26:29 +0800

    :page_facing_up: 使用GPLv3.0协议

--- src/rule/mod.rs
+++ src/rule/mod.rs
@@ -1,1 +1,20 @@
+/*
+    lec A library for creating powerful modern CLI applications. 一个用于创建功能强大的现代CLI应用程序的库
+    Copyright (C) 2023  lontten
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+ */
+
 pub mod command;
